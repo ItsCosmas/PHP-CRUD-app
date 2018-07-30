6334fa82f245f106016f4eeefecc0d7419413d44
ItsCosmas <gikunjucosmas@gmail.com>
2018-07-30 11:43:09 +0300

    Working on a Script to verify form input data

--- backend/signup.php
+++ backend/signup.php
@@ -17,30 +17,33 @@
 <body>
 <div class="container">
     <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 text-left pageBelowNav">
-        <form method="Post" action="register.php">
-            <div class="form-group">
+        <form method="Post" onsubmit=" return Validate()" action="register.php" name="signUp">
+            <div class="form-group" id="fullNameFormGroup">
                 <label for="fullName">Full Name *</label>
-                <input type="text" name="fullName" class="form-control" aria-describedby="Full Name" placeholder="Enter your full Name">
+                <input type="text" name="fullName" class="form-control" aria-describedby="Full Name" placeholder="Enter your full Name" onblur="validateNameDetails()" required>
+                <div id="nameHelp"></div>
             </div>
         <div class="form-group">
             <label for="email">Email *</label>
             <input type="email" class="form-control" name="email" aria-describedby="Email" placeholder="Enter your email">
+            <div id="emailHelp"></div>
         </div>
         <div class="form-group">
                 <label for="username">Username *</label>
                 <input type="text" name="username" class="form-control" aria-describedby="User Name" placeholder="Please Pick a Username">
+                <div id="usernameHelp"></div>
             </div>
         <div class="form-group">
             <label for="password">Password *</label>
-            <input type="password" class="form-control" aria-describedby="Password" name="password" placeholder="Password">
+            <input type="password" class="form-control" id="password" aria-describedby="Password" name="password" placeholder="Password">
         </div>
         <div class="form-group">
             <label for="confirmPassword">Confirm Password *</label>
-            <input type="password" class="form-control" aria-describedby="Confirm Password" name="confirmPassword" placeholder="Re-enter your Password">
+            <input type="password" class="form-control" aria-describedby="Confirm Password" id="confirmPassword" name="confirmPassword" placeholder="Re-enter your Password">
         </div>
-        <input type="checkbox" name="checkbox" style="margin-right:5px;"><label for="check"> I agree to Terms and Conditions</label></br>
+        <input type="checkbox" name="checkbox" style="margin-right:5px;" id="checkbox"><label for="check"> I agree to Terms and Conditions</label></br>
         <button type="submit" name="register" class="btn btn-primary">Register</button>
-        <div style="margin-top:10px">
+        <div style="margin-top:10px" id="errors">
                             <?php 
                             if(isset($errors)){
                                 echo $errors;
@@ -52,4 +55,29 @@
 </div>
 
 </body>
+<script>
+
+// validates password details
+function Validate(){
+    var pass = document.getElementById("password");
+    var confirmPass = document.getElementById("confirmPassword");
+    var errorMessageDiv = document.getElementById("errors");
+    if (pass.value != "" && confirmPass.value != ""){
+        if(pass.value == confirmPass.value){
+            return true;
+        }
+    }
+    errorMessageDiv.innerHTML = "<div class=\"alert alert-warning\"><strong> Both Passwords should Match! </strong> Please try again 😒</div>";
+    return false
+}
+
+// This Function Checks to ensure name is not empty
+function validateNameDetails() {
+    var name = document.forms['signUp']['fullName'].value;
+    var nameHelp = document.getElementById("nameHelp");
+        if (name == ""){
+            nameHelp.innerHTML = "<small id=\"nameHelp\" class=\"form-text text-muted alert alert-warning\">Name Cannot be Empty</small>";
+        }
+}
+</script>
 </html>
